196110b9c461c9962d9a417c45efbe1a56b7a596
ohendriks <hendriks@robot-rose.nl>
2014-10-17 18:51:23 +0200

    Error handling lowlevel fixed lowlevel safety reset stuff
    Former-commit-id: fc2fac0d77091762ebb7ef6328de76fdb762bab2

--- include/shared_variables/update_engine.hpp
+++ include/shared_variables/update_engine.hpp
@@ -153,7 +153,7 @@ public:
 		// The server always returns its local version
 		if( not is_server_ and update_required )
 		{
-			ROS_INFO_NAMED(ROS_NAME, "Timeout, aksing for update to shared variable server.");
+			ROS_DEBUG_NAMED(ROS_NAME, "Timeout, aksing for update to shared variable server.");
 			if(!getRemote())
 				ROS_WARN_NAMED(ROS_NAME, "Could not get shared variable '%s', using old value.", shared_name_.c_str());
 		}
@@ -164,7 +164,7 @@ public:
 	// This function will be invoked by a client in order to get the state of the variable from the server
 	bool getRemote()
 	{
-		ROS_INFO_NAMED(ROS_NAME, "Getting shared variable '%s' using the 'get' service.", shared_name_.c_str());
+		ROS_DEBUG_NAMED(ROS_NAME, "Getting shared variable '%s' using the 'get' service.", shared_name_.c_str());
 		
 		// Check if remote is available
 		if(!remoteAvailable(service_client_get_))
@@ -188,7 +188,7 @@ public:
 	// This function will be invoked by a client in order to change the variable at the server
 	bool setRemote()
 	{
-		ROS_INFO_NAMED(ROS_NAME, "Setting shared variable '%s' using the 'set' service.", shared_name_.c_str());
+		ROS_DEBUG_NAMED(ROS_NAME, "Setting shared variable '%s' using the 'set' service.", shared_name_.c_str());
 
 		// Check if remote is available
 		if(!remoteAvailable(service_client_get_))
@@ -212,7 +212,7 @@ public:
 		if(use_updates_ && ros::Time::now() - prev_publish_time_ > publish_rate_.expectedCycleTime())
         {
         	prev_publish_time_ = ros::Time::now();
-            ROS_INFO_NAMED(ROS_NAME, "Publishing shared variable update via '%s'.", topic_name_updates_.c_str());
+            ROS_DEBUG_NAMED(ROS_NAME, "Publishing shared variable update via '%s'.", topic_name_updates_.c_str());
 			updates_publisher_.publish(ros::conversion::convert<T>().get(value_));
         }
 
